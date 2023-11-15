42bf50d947cddc8f4dd4369056df01dbf0a64bb9
Augusto <gtoborges@gmail.com>
2023-11-15 10:26:44 -0300

    7-composition api basics

---
+++ composition-api/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <h1>{{name}}</h1>
+  <button @click="placeOrder">Place Order</button>
+  <YummyMeal name="Hamburger" :price="5" @addToCart="addItemToCart"/>
+</template>
+
+<script>
+import YummyMeal from './components/YummyMeal.vue'
+import { ref } from 'vue';
+export default {
+  components: { YummyMeal },
+  setup() {
+    const name = ref("The Snazzy Burger");
+    const placeOrder = () => {
+      alert("Your order has been placed!");
+    }
+    const addItemToCart = (item) => {
+      alert(`One ${item} added to the cart`)
+    }
+    return { name, placeOrder, addItemToCart }
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ composition-api/src/components/YummyMeal.vue
@@ -0,0 +1,20 @@
+<template>
+<p>
+  <strong>{{name}}</strong>&nbsp;{{price}}
+  <button @click="addToCart">Add to Cart</button>
+</p>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    price: Number,
+  },
+  setup(props, {emit}) {
+    const addToCart = () => emit("addToCart", props.name)
+    console.log(props.name, props.price)
+    return { addToCart }
+  }
+}
+</script>
