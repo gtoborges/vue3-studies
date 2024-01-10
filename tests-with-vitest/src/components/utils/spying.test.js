3c3eeeb35b803daf3ded56716c9abf35cad77973
Augusto <gtoborges@gmail.com>
2024-01-09 22:37:58 -0300

    add test with spy reset

--- tests-with-vitest/src/components/utils/spying.test.js
+++ tests-with-vitest/src/components/utils/spying.test.js
@@ -8,8 +8,8 @@ test('greeting', () => {
   greeting('Augusto')
 
   expect(spy).toBeCalledTimes(2)
-  expect(spy).toBeCalledWith('Hello, World')
   expect(spy).toBeCalledWith('Hello, Augusto')
+  expect(spy).toBeCalledWith('Hello, World')
 })
 
 test('greeting specific order', () => {
@@ -21,4 +21,17 @@ test('greeting specific order', () => {
   expect(spy).toBeCalledTimes(2)
   expect(spy.mock.calls[0][0]).toBe('Hello, World')
   expect(spy.mock.calls[1][0]).toBe('Hello, Augusto')
+})
+
+test('greeting separately', () => {
+  const spy = vi.spyOn(console, 'log')
+
+  greeting('World')
+  expect(spy).toBeCalledTimes(1)
+  expect(spy.mock.calls[0][0]).toBe('Hello, World')
+
+  spy.mockReset()
+  greeting('Augusto')
+  expect(spy).toBeCalledTimes(1)
+  expect(spy.mock.calls[0][0]).toMatchInlineSnapshot(`"Hello, Augusto"`)
 })
