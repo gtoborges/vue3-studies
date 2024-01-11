a5632b11a59cf2dda2394d5b5f3fa2ef35e96790
Augusto <gtoborges@gmail.com>
2024-01-10 22:51:01 -0300

    add simple time mock

--- tests-with-vitest/src/components/utils/mocks.test.js
+++ tests-with-vitest/src/components/utils/mocks.test.js
@@ -1,6 +1,10 @@
 import { test, expect, vi } from 'vitest'
 import { upperCaseName } from './greeting';
 
+function getCurrentTime() {
+  return new Date().toTimeString().slice(0, 5);
+}
+
 vi.mock('lodash', (importOriginal) => {
   const actual = importOriginal()
   return {
@@ -14,4 +18,11 @@ vi.mock('lodash', (importOriginal) => {
 test('upperFirstCaseName', () => {
   const name = upperCaseName()
   expect(name).toEqual('Gtoborges')
+})
+
+test('time', () => {
+  vi.setSystemTime(new Date('2000-1-1 22:34'))
+  expect(getCurrentTime()).toBe('22:34')
+
+  vi.useRealTimers()
 })
