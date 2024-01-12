6e898d4d5d4795b7f0b61f977c517e4b7768e5c7
Augusto <gtoborges@gmail.com>
2024-01-12 10:45:54 -0300

    split network test into setup file

---
+++ tests-with-vitest/src/components/utils/msw.setup.js
@@ -0,0 +1,11 @@
+import { setupServer } from 'msw/node'
+import { http, HttpResponse } from 'msw'
+
+const server = setupServer(
+  http.get(
+    "https://jsonplaceholder.typicode.com/post/:id",
+    ({ params }) => {
+      return HttpResponse.json({body: `mocked${params.id}`})
+    }
+  )
+)
