0099f629d3a7bb7f0a17772e857a8339a53bd061
Augusto <gtoborges@gmail.com>
2024-01-16 15:58:36 -0300

    add reactive property

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -1,23 +1,22 @@
 <template>
   <h1>{{name}}</h1>
   <button @click="placeOrder">Place Order</button>
-  <YummyMeal name="Hamburger" :price="5" @addToCart="addItemToCart"/>
+  <YummyMeal :name="meal.name" :price="meal.price" @addToCart="addItemToCart"/>
 </template>
 
 <script>
 import YummyMeal from './components/YummyMeal.vue'
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 export default {
   components: { YummyMeal },
   setup() {
     const name = ref("The Snazzy Burger");
-    const placeOrder = () => {
-      alert("Your order has been placed!");
-    }
-    const addItemToCart = (item) => {
-      alert(`One ${item} added to the cart`)
-    }
-    return { name, placeOrder, addItemToCart }
+    const meal = reactive({ name: "Hamburger", price: 5})
+
+    const placeOrder = () => alert("Your order has been placed!");
+    const addItemToCart = (item) => alert(`One ${item} added to the cart`)
+
+    return { name, placeOrder, addItemToCart, meal }
   }
 }
 </script>
