8c0e386a73b5ecf5221c230a8e8f9b60d2709b3c
Augusto <gtoborges@gmail.com>
2024-01-16 16:02:38 -0300

    add computed property on options api

--- composition-api/src/components/YummyMeal.vue
+++ composition-api/src/components/YummyMeal.vue
@@ -1,20 +1,26 @@
 <template>
 <p>
-  <strong>{{name}}</strong>&nbsp;{{price}}
+  <strong>{{name}}</strong>&nbsp;{{priceFormat}}
   <button @click="addToCart">Add to Cart</button>
 </p>
 </template>
 
 <script>
+// import { computed } from 'vue'
+
 export default {
   props: {
     name: String,
     price: Number,
   },
   setup(props, {emit}) {
-    const addToCart = () => emit("addToCart", props.name)
-    console.log(props.name, props.price)
-    return { addToCart }
+    const addToCart = () => emit("addToCart", props.name);
+    return { addToCart };
+  },
+  computed: {
+    priceFormat() {
+      return `$${this.price.toFixed(2)}`;
+    }
   }
 }
 </script>
