b4ba7d031d71c9e4a0c0cd024505a9b5cdbec434
Augusto <gtoborges@gmail.com>
2024-02-09 22:54:05 -0300

    add dinamic routes

---
+++ nuxt3-fundamentals/pages/movies/[id].vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>{{$route.params.id}}</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ nuxt3-fundamentals/pages/teams/[teamSlug]/users/[userId].vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const route = useRoute()
+const message = computed(() => `I'm on team ${route.params.teamSlug}, with userId ${route.params.userId}`)
+</script>
+
+<template>
+  <div>
+    <h1>{{message}}</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
