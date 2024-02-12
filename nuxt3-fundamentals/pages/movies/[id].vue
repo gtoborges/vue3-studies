b13cd5db50ceae27c782e75febef4f3949797283
Augusto <gtoborges@gmail.com>
2024-02-12 10:54:49 -0300

    change useAsyncData to useFetch hook

--- nuxt3-fundamentals/pages/movies/[id].vue
+++ nuxt3-fundamentals/pages/movies/[id].vue
@@ -1,13 +1,23 @@
 <script lang="ts" setup>
 const route = useRoute()
 
-const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
-  return $fetch(
-    `https://www.omdbapi.com/`, 
-    { params: { apikey: 'bd55fcf1', i: route.params.id } }
-  );
+const { data } = useFetch(`https://www.omdbapi.com/`, {
+  params: { apikey: 'bd55fcf1', i: route.params.id },
+  pick: ['Title', 'Plot'],
+  key: `/movies/${route.params.id}`,
 })
-console.log(data)
+
+// const { data } = useAsyncData(`/movies/${route.params.id}`, () => {
+//   return $fetch(`https://www.omdbapi.com/`, { params: { apikey: 'bd55fcf1', i: route.params.id } });
+// }, {
+//   pick: ['Title', 'Plot'],
+//   // transform(data) {
+//   //   return {
+//   //     Title: data.Title,
+//   //     Plot: data.Plot,
+//   //   }
+//   // }
+// })
 </script>
 
 <template>
