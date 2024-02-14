c83d2bb7c4913b9553e192fcb859f361a77e1fd6
Augusto <gtoborges@gmail.com>
2024-02-14 19:02:07 -0300

    add dinamic title and metatags with useHead hook

--- nuxt3-fundamentals/app.vue
+++ nuxt3-fundamentals/app.vue
@@ -1,3 +1,10 @@
+<script setup>
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - My movie site` : 'My movie site'
+  }
+})
+</script>
 <template>
   <div>
     <nav style="display:flex; gap:10px;">
--- nuxt3-fundamentals/pages/movies/[id].vue
+++ nuxt3-fundamentals/pages/movies/[id].vue
@@ -1,15 +1,26 @@
 <script lang="ts" setup>
 const route = useRoute()
 
-const { data, error } = useFetch(`https://www.omdbapi.com/`, { // api retorna status 200 com erro
+const { data, error } = await useFetch(`https://www.omdbapi.com/`, { // api retorna status 200 com erro
   params: { apikey: 'bd55fcf1', i: route.params.id },
-  pick: ['Title', 'Plot', 'Error'],
+  pick: ['Title', 'Plot', 'Poster', 'Error'],
   key: `/movies/${route.params.id}`,
 })
 
 if(data.value && data.value['Error'] === "Incorrect IMDb ID.") {
   showError({status: 404, statusMessage: "Page not found"});
 }
+
+
+useHead({
+  title: data.value.Title,
+  meta: [
+    { name: "description", content: data.value.Plot },
+    { property: "og:description", content: data.value.Plot },
+    { property: "og:image", content: data.value.Poster },
+    { name: "twitter:card", content: `summary_large_image` },
+  ]
+})
 </script>
 
 <template>
