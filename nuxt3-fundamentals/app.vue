4643c9cde2868249d10da7011b0bf68ef59ef512
Augusto <gtoborges@gmail.com>
2024-02-15 21:49:05 -0300

    creating custom layouts for different pages

--- nuxt3-fundamentals/app.vue
+++ nuxt3-fundamentals/app.vue
@@ -6,11 +6,7 @@ useHead({
 })
 </script>
 <template>
-  <div>
-    <nav style="display:flex; gap:10px;">
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/movies">Movies</NuxtLink>
-    </nav>
-    <NuxtPage/>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
