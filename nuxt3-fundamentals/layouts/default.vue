e34d288483f8ea7a4f459e46a89202795a0482ce
Augusto <gtoborges@gmail.com>
2024-02-15 22:59:20 -0300

    add global middleware logging navigation routes

--- nuxt3-fundamentals/layouts/default.vue
+++ nuxt3-fundamentals/layouts/default.vue
@@ -2,7 +2,7 @@
   <nav style="display:flex; gap:10px;">
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/movies">Movies</NuxtLink>
-    teste
+    <NuxtLink to="/login">Login</NuxtLink>
   </nav>
   <slot></slot>
 </template>
