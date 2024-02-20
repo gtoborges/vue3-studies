7661cca2b73f7f8184a9cc5e9ad30144d09aa5ab
Augusto <gtoborges@gmail.com>
2024-02-19 22:52:18 -0300

    create composable with useState to simulate login with state management

--- nuxt3-fundamentals/layouts/default.vue
+++ nuxt3-fundamentals/layouts/default.vue
@@ -1,8 +1,12 @@
+<script setup>
+const isLoggedIn = useIsLoggedIn();
+</script>
 <template>
   <nav style="display:flex; gap:10px;">
     <NuxtLink to="/">Home</NuxtLink>
     <NuxtLink to="/movies">Movies</NuxtLink>
-    <NuxtLink to="/login">Login</NuxtLink>
+    <NuxtLink to="/login" v-if="!isLoggedIn">Login</NuxtLink>
+    <a href="#" v-else @click.prevent="isLoggedIn = false">Logout</a>
   </nav>
   <slot></slot>
 </template>
