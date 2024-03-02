3b75f7cec8c7a0a17de6abe8ea0d74a4ea933ca2
Augusto <gtoborges@gmail.com>
2024-03-01 22:08:57 -0300

    checking different rendering modes and route rules

--- nuxt3-fundamentals/nuxt.config.ts
+++ nuxt3-fundamentals/nuxt.config.ts
@@ -15,4 +15,9 @@ export default defineNuxtConfig({
   alias: {
     pinia: "/node_modules/@pinia/nuxt/node_modules/pinia/dist/pinia.mjs",
   },
+  routeRules: {
+    "/spa": { ssr: false }, // renders only on client-side
+    "/static": { static: true }, // static after generated on first visit
+    "/swr": { swr: true }, // page generated on demand, revalidates in background, cached until API response changes (stale while revalidate)
+  }
 })
