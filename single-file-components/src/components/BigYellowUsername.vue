8c27bf4f958b1f2c110bb18ca74397423f3f8bec
Augusto <gtoborges@gmail.com>
2024-03-04 21:43:57 -0300

    scaffold project and add simple single file component

---
+++ single-file-components/src/components/BigYellowUsername.vue
@@ -0,0 +1,19 @@
+<template>
+  <p class="username">{{ username }}</p>
+</template>
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+<style>
+.username {
+  font-size: 50pt;
+  color: #dcae1d;
+}
+</style>
