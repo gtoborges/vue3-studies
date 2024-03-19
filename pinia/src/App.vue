f370870ae3de4a2a005277153fa886ee1a6923e8
Augusto <gtoborges@gmail.com>
2024-03-18 22:29:03 -0300

    products list populated with async json import

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -4,6 +4,7 @@ import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
 
 const productStore = useProductStore()
+productStore.fill()
 </script>
 
 <template>
