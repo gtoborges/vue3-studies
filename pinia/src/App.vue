e3f847b19946a3f61b73963c5d7ccc2dd42305d3
Augusto <gtoborges@gmail.com>
2024-03-27 22:29:37 -0300

    implements history to cartStore state with subcribe and undo function

--- pinia/src/App.vue
+++ pinia/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
+import { ref, reactive } from "vue";
 import { useProductStore } from "@/stores/ProductStore";
 import { useCartStore } from "@/stores/CartStore";
 
@@ -8,6 +9,24 @@ const productStore = useProductStore()
 productStore.fill()
 
 const cartStore = useCartStore()
+
+const history = reactive([])
+history.push(JSON.stringify(cartStore.$state))
+const doingHistory = ref(false)
+
+const undo = () => {
+  if(history.length === 1) return
+
+  doingHistory.value = true
+  history.pop()
+  cartStore.$state = JSON.parse(history.at(-1))
+  doingHistory.value = false
+}
+
+cartStore.$subscribe( (mutation, state) => {
+  if(!doingHistory.value) history.push(JSON.stringify(state))
+})
+
 cartStore.$onAction( ({name, store, args, after, onError }) => {
   if(name === 'addItems') {
     after( () => {
@@ -20,6 +39,9 @@ cartStore.$onAction( ({name, store, args, after, onError }) => {
 <template>
   <div class="container">
     <TheHeader />
+    <div class="mb-5 flex justify-end">
+      <AppButton @click="undo">Undo</AppButton>
+    </div>
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
         v-for="product in productStore.products"
